43593470d8a09d019cc4386ad493cf23a7f4224e
Joachim Forsberg <j5m@gmx.com>
2021-11-11 16:07:33 +0100

    Added Boat class and methods in player class

--- src/com/edugrade/battleship/Boats.java
+++ src/com/edugrade/battleship/Boats.java
@@ -1,4 +1,48 @@
 package com.edugrade.battleship;
 
+/**
+* Boat objects for the battleship game.
+* This class provides the basic building blocks to create Boat objects.
+* This class doesn't need to be instantiated because it's using a factory method to call the constructor.
+* @author Joachim Forsberg
+* @version 1.0
+* @since 2021-11-10
+* */
+
 public class Boats {
+    private String shipType;
+    private int numOfSquares;
+    private String[] locationOnMap;
+
+    /**
+     * Constructor
+     * */
+    public Boats(String shipType, int numOfSquares, String[] locationOnMap) {
+        this.shipType = shipType;
+        this.numOfSquares = numOfSquares;
+        this.locationOnMap = locationOnMap;
+    }
+
+    /**
+     * Getters
+     * */
+    public String[] getLocationOnMap() {
+        return locationOnMap;
+    }
+
+    public String getShipType() {
+        return shipType;
+    }
+
+    public int getNumOfSquares() {
+        return numOfSquares;
+    }
+
+    /**
+     * Factory method that calls the constructor, which allows us to create Boat
+     * objects without the need to instantiate them.
+     * */
+    public static Boats createBoat(String shipType, int numOfSquares, String[] locationOnMap) {
+        return new Boats(shipType, numOfSquares, locationOnMap);
+    }
 }
--- src/com/edugrade/battleship/Player.java
+++ src/com/edugrade/battleship/Player.java
@@ -1,10 +1,110 @@
 package com.edugrade.battleship;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Player {
     private static GameMap map = new GameMap();
     private static Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<Boats> fleet;
+
+    public Player() {
+        this.fleet = new ArrayList<>();
+    }
+
+    /**
+     * This method is used to create Boat objects and add them to the fleet list.
+     * @author Joachim Forsberg
+     * */
+    public void createFleet() {
+        fleet.add(Boats.createBoat("Carrier", 5, new String[] {"D4", "E4", "F4", "G4", "H4"}));
+        fleet.add(Boats.createBoat("Battleship", 4, new String[] {"A4", "A5", "A6", "A7"}));
+        fleet.add(Boats.createBoat("Battleship", 4, new String[] {"J0", "J1", "J2", "J3"}));
+        fleet.add(Boats.createBoat("Cruiser", 3, new String[] {"D2", "E2", "F2"}));
+        fleet.add(Boats.createBoat("Cruiser", 3, new String[] {"C6", "D6", "E6"}));
+        fleet.add(Boats.createBoat("Cruiser", 3, new String[] {"J5", "J6", "J7"}));
+        fleet.add(Boats.createBoat("Submarine", 2, new String[] {"H9", "I9"}));
+        fleet.add(Boats.createBoat("Submarine", 2, new String[] {"C9", "D9"}));
+        fleet.add(Boats.createBoat("Submarine", 2, new String[] {"G6", "G7"}));
+        fleet.add(Boats.createBoat("Submarine", 2, new String[] {"C0", "D0"}));
+    }
+
+    /**
+     * This method is used to merge all the locations of the whole fleet in a single ArrayList.
+     * @author Joachim Forsberg
+     * */
+    public ArrayList<String> getFleetLocation() {
+        ArrayList<String> locations = new ArrayList<>();
+        for (int i = 0; i < this.fleet.size(); i++) {
+            locations.addAll(Arrays.asList(this.fleet.get(i).getLocationOnMap()));
+        }
+        return locations;
+    }
+
+    /**
+     * This is the updated method to place the fleet on the game map. Which
+     * uses the locations from the Boat objects.
+     * @author Joachim Forsberg
+     * @param location Is the ArrayList of all the fleet's locations.
+     * */
+    public void placeFleetOnMap(ArrayList<String> location) {
+
+        /** The Fleet's location
+         * [-], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+         * [A],  ,  ,  ,  , @, @, @, @,  ,  ]
+         * [B],  ,  ,  ,  ,  ,  ,  ,  ,  ,  ]
+         * [C], @,  ,  ,  ,  ,  , @,  ,  , @]
+         * [D], @,  , @,  , @,  , @,  ,  , @]
+         * [E],  ,  , @,  , @,  , @,  ,  ,  ]
+         * [F],  ,  , @,  , @,  ,  ,  ,  ,  ]
+         * [G],  ,  ,  ,  , @,  , @, @,  ,  ]
+         * [H],  ,  ,  ,  , @,  ,  ,  ,  , @]
+         * [I],  ,  ,  ,  ,  ,  ,  ,  ,  , @]
+         * [J], @, @, @, @,  , @, @, @,  ,  ]
+         * */
+
+        String letter = "";
+        int number;
+
+        for (int i = 0; i < location.size(); i++) {
+            letter = location.get(i).substring(0,1);
+            number = Integer.parseInt(location.get(i).substring(1));
+            switch (letter) {
+                case "A":
+                    map.setIndexValue("@", 1, number+1);
+                    break;
+                case "B":
+                    map.setIndexValue("@", 2, number+1);
+                    break;
+                case "C":
+                    map.setIndexValue("@", 3, number+1);
+                    break;
+                case "D":
+                    map.setIndexValue("@", 4, number+1);
+                    break;
+                case "E":
+                    map.setIndexValue("@", 5, number+1);
+                    break;
+                case "F":
+                    map.setIndexValue("@", 6, number+1);
+                    break;
+                case "G":
+                    map.setIndexValue("@", 7, number+1);
+                    break;
+                case "H":
+                    map.setIndexValue("@", 8, number+1);
+                    break;
+                case "I":
+                    map.setIndexValue("@", 9, number+1);
+                    break;
+                case "J":
+                    map.setIndexValue("@", 10, number+1);
+                    break;
+            }
+        }
+    }
 
     public int[] shoot(){
         int[] shot = new int[2];
@@ -102,10 +202,18 @@ public class Player {
         map.setIndexValue("@",4,10);
     }
 
+    /**
+     * This method generates the Player game map.
+     * @author Joachim Forsberg
+     * */
     public void drawPlayerMap() {
         map.drawMap();
     }
 
+    /**
+     * This method prints the Player game map to the terminal.
+     * @author Joachim Forsberg
+     * */
     public void printPlayerMap() {
         map.printMap();
     }
