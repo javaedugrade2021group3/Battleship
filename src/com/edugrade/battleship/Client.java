f3328a7d6cb6f823823397aba8c4c9657753e2c8
Kadahizi <jbatgoal@yahoo.fr>
2021-11-15 23:01:10 +0100

    communication between server and client

--- src/com/edugrade/battleship/Client.java
+++ src/com/edugrade/battleship/Client.java
@@ -1,9 +1,56 @@
 package com.edugrade.battleship;
 
-/* Created a class for client
-@author Ivan
- */
-
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
+    public  static  void main(String[]args){
+        Socket socket= null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+
+        try {
+            socket = new Socket("localhost",3030);
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            System.out.println("Write some thing to Start conversation with server! :");
+            Scanner scanner = new Scanner(System.in);
+
+            while (true){
+                String msgTosend = scanner.nextLine();
+                bufferedWriter.write(msgTosend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                String textServerWelcome = null;
+                System.out.println("Grupp 3 server : "+ bufferedReader.readLine() );
+                if(msgTosend.equalsIgnoreCase("quit")){
+                    break;
+                }
+            }
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+           try {
+               if(socket!=null){
+                   socket.close();
+               }
+               if(bufferedReader!=null){
+                   bufferedReader.close();
+               }
+               if(bufferedWriter!=null){
+                   bufferedWriter.close();
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+        }
+    }
+
 }
--- src/com/edugrade/battleship/Server.java
+++ src/com/edugrade/battleship/Server.java
@@ -1,9 +1,58 @@
 package com.edugrade.battleship;
 
-/* Created class for server
-@author Ivan
- */
-
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    public static  void  main(String[] args) throws IOException {
+        Socket socket= null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+        serverSocket = new ServerSocket(3030);
+
+        while (true){
+            try {
+                socket= serverSocket.accept();
+
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+                if(socket!=null){
+                    bufferedWriter.write(showOptions("Welcome to group 3 server,"));
+                    //här ska man visa game instructions ,start options
+                }
+                while (true){
+                    String msgFromClient = bufferedReader.readLine();
+                    System.out.println("Client sent message: " + msgFromClient);
+                    //här ska man börja kolla va client skriver och skicka svar
+                    bufferedWriter.write("Well received");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                    if(msgFromClient.equalsIgnoreCase("quit")){
+                        bufferedWriter.write("Bye see u next time");
+                        break;
+                    }
+                }
+                socket.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+            } catch (IOException e){
+             e.printStackTrace();
+            }
+
+        }
+
+
+    }
+    public static String showOptions(String message){
+        return message;
+    }
 }
+
